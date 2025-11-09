3804eb65b98c874b192d016f626cb2eaed14da13
Allen Porter <allen.porter@gmail.com>
2025-11-09 18:00:27 +0000

    Partial revert "Update config flow"
    This reverts commit e6772785a5b12b7aa6d67bc472b341f8f8cafee5.

--- custom_components/supernote_cloud/config_flow.py
+++ custom_components/supernote_cloud/config_flow.py
@@ -8,11 +8,12 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigFlowResult, SOURCE_REAUTH
+from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.helpers import selector
-from homeassistant.helpers import config_entry_oauth2_flow
 from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaCommonFlowHandler,
+    SchemaConfigFlowHandler,
+    SchemaFlowFormStep,
     SchemaFlowError,
 )
 from homeassistant.util import dt as dt_util
@@ -38,21 +39,14 @@ from .supernote_client.exceptions import SupernoteException, ApiException
 _LOGGER = logging.getLogger(__name__)
 
 
-class SupernoteConfigFlowError(SchemaFlowError):
-    """Custom error for Supernote Cloud config flow."""
-
-    def __init__(self, error_key: str) -> None:
-        """Initialize SupernoteConfigFlowError."""
-        super().__init__(error_key)
-        self.error_key = error_key
-
-
 async def validate_user_input(
-    hass: HomeAssistant,
-    user_input: dict[str, Any],
+    handler: SchemaCommonFlowHandler, user_input: dict[str, Any]
 ) -> dict[str, Any]:
     """Validate user input."""
-    websession = async_get_clientsession(hass)
+    handler.parent_handler._async_abort_entries_match(  # noqa: SLF001
+        {CONF_USERNAME: user_input[CONF_USERNAME]}
+    )
+    websession = async_get_clientsession(handler.parent_handler.hass)
     login_client = LoginClient(Client(websession))
     try:
         access_token = await login_client.login(
@@ -60,7 +54,7 @@ async def validate_user_input(
         )
     except (ApiException, SupernoteException) as err:
         _LOGGER.debug("Login api exception: %s", err)
-        raise SupernoteConfigFlowError("api_error") from err
+        raise SchemaFlowError("api_error") from err
 
     # Verify the API works
     supernote_client = SupernoteCloudClient(
@@ -70,7 +64,9 @@ async def validate_user_input(
         user_response = await supernote_client.query_user(user_input[CONF_USERNAME])
     except (ApiException, SupernoteException) as err:
         _LOGGER.debug("Query api exception: %s", err)
-        raise SupernoteConfigFlowError("api_error") from err
+        raise SchemaFlowError("api_error") from err
+    await handler.parent_handler.async_set_unique_id(unique_id=str(user_response.user_id))  # type: ignore[union-attr]
+    handler.parent_handler._abort_if_unique_id_configured()  # type: ignore[union-attr]
     return {
         **user_input,
         CONF_ACCESS_TOKEN: access_token,
@@ -81,25 +77,34 @@ async def validate_user_input(
     }
 
 
-USER_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_USERNAME): selector.TextSelector(
-            selector.TextSelectorConfig(),
+CONFIG_FLOW = {
+    "user": SchemaFlowFormStep(
+        schema=vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): selector.TextSelector(
+                    selector.TextSelectorConfig(),
+                ),
+                vol.Required(CONF_PASSWORD): selector.TextSelector(
+                    selector.TextSelectorConfig(
+                        type=selector.TextSelectorType.PASSWORD
+                    ),
+                ),
+            }
         ),
-        vol.Required(CONF_PASSWORD): selector.TextSelector(
-            selector.TextSelectorConfig(type=selector.TextSelectorType.PASSWORD),
-        ),
-    }
-)
+        validate_user_input=validate_user_input,
+    )
+}
+
+OPTIONS_FLOW = {
+    "init": SchemaFlowFormStep(),
+}
 
 
-class SupernoteCloudConfigFlowHandler(
-    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
-):
+class SupernoteCloudConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
     """Handle a config flow for Switch as X."""
 
-    DOMAIN = DOMAIN
-    logger = _LOGGER
+    config_flow = CONFIG_FLOW
+    options_flow = OPTIONS_FLOW
 
     VERSION = 1
     MINOR_VERSION = 1
@@ -125,39 +130,14 @@ class SupernoteCloudConfigFlowHandler(
             )
         return await self.async_step_user()
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> ConfigFlowResult:
-        """Handle the initial step."""
-        errors: dict[str, str] | None = None
-        if user_input is not None:
-            self._async_abort_entries_match(  # noqa: SLF001
-                {CONF_USERNAME: user_input[CONF_USERNAME]}
-            )
-
-            try:
-                data = await validate_user_input(self.hass, user_input)
-            except SupernoteConfigFlowError as err:
-                errors = {"base": err.error_key}
-            else:
-                await self.async_set_unique_id(unique_id=data[CONF_UNIQUE_ID])
-                self._abort_if_unique_id_configured()  # type: ignore[union-attr]
-
-                return self.async_create_entry(
-                    title=data[CONF_API_USERNAME], data={}, options=data
-                )
-        return self.async_show_form(
-            step_id="user", data_schema=USER_SCHEMA, errors=errors
-        )
-
-    async def async_oauth_create_entry(self, data: dict) -> ConfigFlowResult:
-        """Create an oauth config entry or update existing entry for reauth."""
-        data[CONF_TOKEN_TIMESTAMP] = dt_util.now().timestamp()
-        if self.source == SOURCE_REAUTH:
-            self._abort_if_unique_id_mismatch()
-            return self.async_update_reload_and_abort(
-                self._get_reauth_entry(),
-                data_updates=data,
-            )
-        self._abort_if_unique_id_configured()
-        return await super().async_oauth_create_entry(data)  # type: ignore[no-any-return]
+    # async def async_oauth_create_entry(self, data: dict) -> ConfigFlowResult:
+    #     """Create an oauth config entry or update existing entry for reauth."""
+    #     data[CONF_TOKEN_TIMESTAMP] = dt_util.now().timestamp()
+    #     if self.source == SOURCE_REAUTH:
+    #         self._abort_if_unique_id_mismatch()
+    #         return self.async_update_reload_and_abort(
+    #             self._get_reauth_entry(),
+    #             data_updates=data,
+    #         )
+    #     self._abort_if_unique_id_configured()
+    #     return await super().async_oauth_create_entry(data)  # type: ignore[no-any-return]
